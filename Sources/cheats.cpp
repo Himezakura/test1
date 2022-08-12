dfaf5f33f2907383e461fecb7c885dfa2fd40042
Himezakura <89678734+Himezakura@users.noreply.github.com>
2022-08-13 02:03:04 +0900

    Add files via upload

---
+++ Sources/cheats.cpp
@@ -0,0 +1,25 @@
+#include "cheats.hpp"
+
+namespace CTRPluginFramework
+{
+	/* 記述例
+	Process::Write32(0x00000000, 0x00000000); 32bit書き込み
+	Process::Write16(0x00000000, 0x0000); 16bit書き込み
+	Process::Write8(0x00000000, 0x00); 8bit書き込み
+	
+	Process::Read32(0x00000000, data); 32bit読み込み
+	Process::Read16(0x00000000, data); 16bit読み込み
+	Process::Read8(0x00000000, data); 8bit読み込み
+	
+	ADDFLOAT(0x00000000, 0.1f) moonjumpや座標移動(加算)
+	SUBFLOAT(0x00000000, 0.1f) moonjumpや座標移動(減算)
+	
+	if (Controller::IsKeysDown(A))　{} ボタン実行
+	*/
+
+
+
+
+
+
+}
